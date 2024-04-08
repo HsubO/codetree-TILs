9b46f5dea32aa5f1fa9d2b3bc82c5ca5fd0680d3
HsubO <162892753+HsubO@users.noreply.github.com>
2024-04-08 17:43:16 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a =3;
+    int b = 5;
+    int k = a;
+    a =b;
+    b =k;
+    cout << a<<"\n"<<b;
+    return 0;
+}
