a399dcbef4fd7532b904a373c982efd5b3dafd5d
HsubO <162892753+HsubO@users.noreply.github.com>
2024-04-08 17:00:23 +0900

    Title: 두줄 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/두줄 출력/print-two-sentences-introduce.cpp
+++ 240408/두줄 출력/print-two-sentences-introduce.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cout<<"Heelo students!"<<endl;
+    cout<<"Hello students!"<<endl;
     cout<<"Welcome to LeebrosCode!";
     return 0;
 }
